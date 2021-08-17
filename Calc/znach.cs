7dcf9b3f433604e158569a5740869c7055c7c175
Антон Бойко <energyone.tony@mail.ru>
2021-08-17 13:35:38 +0300

    Создание класса для упрощения кода

---
+++ Calc/znach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class znach: MainWindow
+    {
+        int z1 = 0;
+        int z2 = 0;
+
+        public void Text ()
+        {
+            int t1 = Convert.ToInt32(text1.Text);
+            int t2 = Convert.ToInt32(text2.Text);
+        }
+
+        public void Znach(int znach1, int znach2)
+        {
+            z1 = znach1;
+            z2 = znach2;
+        }
+    }
+}
