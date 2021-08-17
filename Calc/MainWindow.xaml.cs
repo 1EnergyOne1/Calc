3273572cb3e646f54ddfb2cc5684854eef643e98
Антон Бойко <energyone.tony@mail.ru>
2021-08-17 11:29:01 +0300

    Add project files.

---
+++ Calc/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Star(object sender, RoutedEventArgs e)
+        {
+            int t1 = Convert.ToInt32(text1.Text);
+            int t2 = Convert.ToInt32(text2.Text);        
+
+            Result.Content = t1 * t2;
+        }
+
+        private void Button_Click_Cherta(object sender, RoutedEventArgs e)
+        {
+            int t1 = Convert.ToInt32(text1.Text);
+            int t2 = Convert.ToInt32(text2.Text);
+
+            Result.Content = t1 / t2;
+        }
+
+        private void Button_CLick_Plus(object sender, RoutedEventArgs e)
+        {
+            int t1 = Convert.ToInt32(text1.Text);
+            int t2 = Convert.ToInt32(text2.Text);
+
+            Result.Content = t1 + t2;
+        }
+
+        private void Button_Click_Minus(object sender, RoutedEventArgs e)
+        {
+            int t1 = Convert.ToInt32(text1.Text);
+            int t2 = Convert.ToInt32(text2.Text);
+
+            Result.Content = t1 - t2;
+        }
+    }
+}
